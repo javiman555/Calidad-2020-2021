9fd5030a57c1fb25e87c53a0a82896f775c58f59
IvanMGM <ivanmgm88@gmail.com>
2021-05-07 20:35:04 +0200

    Cucumber

---
+++ app/src/test/java/gradle/cucumber/StepDefinitions_HU1.java
@@ -0,0 +1,28 @@
+package gradle.cucumber;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.When;
+import cucumber.api.java.en.Then;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+public class StepDefinitions_HU1 {
+
+    @Given("^I am Senior Citicen$")
+    public void i_am_senior_citicen() throws Throwable {
+        throw new PendingException();
+    }
+
+    @When("^I click play$")
+    public void i_click_play() throws Throwable {
+        throw new PendingException();
+    }
+
+    @Then("^I access the games$")
+    public void i_access_the_games() throws Throwable {
+        throw new PendingException();
+    }
+
+}
---
+++ app/src/test/resources/gradle/cucumber/HU_1SeniorJuega.feature
@@ -0,0 +1,8 @@
+Feature: Play Games
+  As a Senior Citicen
+  I want to have access to games
+  Because I want to play games
+  Scenario: Access Games
+    Given I am Senior Citicen
+    When I click play
+    Then I access the games
